4c722b6ee6e7b72ac61da1520cb7363535c66e22
Yu Bo <yubo@harmony.com>
2017-09-13 12:23:52 +0800

    A

---
+++ debug/post_req.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding:gbk -*-
+import sys
+import re
+import os
+import string
+import urllib
+import urllib2
+import datetime
+import json
+from bs4 import BeautifulSoup
+
+
+#urlall = "http://www.cninfo.com.cn/cninfo-new/memo-2"
+#urlall = "http://www.cninfo.com.cn/information/companyinfo_n.html?fulltext?szmb000615"
+urlall = "http://www.cninfo.com.cn/cninfo-new/disclosure/szse/fulltext"
+filename = 'debug/_html.txt'
+
+values = {'stock':'300418','searchkey':'','category':'','pageNum':'1','pageSize':'15','column':'szse_gem','tabName':'latest','sortName':'','sortType':'','limit':'','seDate':''}
+data = urllib.urlencode(values)
+
+tf_fl = open(filename, 'w+')
+try:
+	#req : urllib2.Request(urlall)
+	res_data = urllib2.urlopen(urlall, data)
+except:
+	print "Error fupai urlopen"
+	#LOOP_COUNT = LOOP_COUNT+1
+if res_data is None:
+	print "Open URL fail"
+	exit(0)
+
+content = res_data.read()
+tf_fl.write(content)
+
+s = json.loads(content)
+obj = s['classifiedAnnouncements']
+print (len(obj))
+print obj[0][0]
+print type(obj[0][0])
+print s.keys()
+
+'''
+line = res_data.readline()
+while line:
+	try:
+		tf_fl.write(line)
+	except:
+		#print "?????????????",line.decode('utf8')
+		tf_fl.write(line)
+	line = res_data.readline()
+'''
+tf_fl.close()
+
