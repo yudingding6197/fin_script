a60fa22a9f87a6b3a0fbe6a317ec9db0cdd9d8b5
Bo Yu <yubo@kyntos.cn>
2019-03-29 17:37:50 +0800

    A

--- debug/guben.py
+++ debug/guben.py
@@ -13,7 +13,18 @@ sys.path.append(".")
 #sys.path.append("..")
 from internal.common_inf import *
 
+def digit_full_with_space(digit, length):
+	#fmt = "%" + ("%d"%(length)) + "d"
+	fmt = "%%%dd"%(length)
+	d_v = fmt%(digit)
+	return d_v
+def full_with_space(digit, length):
+	fmt = "%" + ("%d"%(length)) + "d"
+	d_v = fmt%(digit)
+	return d_v
+
 def handle_guben(code, src):
+	title = ['变动日期','总股本','流通A股','高管股','限售A股']
 	content = get_guben_change(code, src)
 	#print(content)
 	if content is None:
@@ -24,15 +35,41 @@ def handle_guben(code, src):
 	if not os.path.exists(folder):
 		os.makedirs(folder)
 
+	s_folder = os.path.join(predir, '_summar')
+	if not os.path.exists(s_folder):
+		os.makedirs(s_folder)
+
 	location = os.path.join(folder, '_guben.txt')
 	f = open(location, 'w+')
 	#写入最新更新日期 和 飞线数量
-	f.write(content[0][0] + '\n')
-	f.write(str(content[1][0]) + '\n')
-	for item in content:
-		for val in item:
-			f.write(str(val) + ' ')
+	value = "%s,%d\n"%(content[0][0], content[1][0])
+	f.write(value)
+
+	#count = len(content[0])
+	#line_fmt = '10% '
+	#for i in range(0, count):
+	#	line_fmt += '12% '
+	line_idx = 0
+	date_len = len(content[0][0]) + 1
+	for line_obj in content:
+		b_first = 1
+		for item in line_obj:
+			if line_idx==0:
+				if b_first:
+					value = '%-6s %11s'%(title[line_idx].decode('gbk'), item)
+					b_first = 0
+				else:
+					value = ' ' + item
+			else:
+				if b_first:
+					value = '%-6s %12d'%(title[line_idx].decode('gbk'), item)
+					l = value.encode('gbk')
+					b_first = 0
+				else:
+					value = digit_full_with_space(item, date_len)
+			f.write(value.encode('gbk'))
 		f.write('\n')
+		line_idx += 1
 	f.close()
 
 def usage_help():
@@ -47,7 +84,6 @@ param_config = {
 	"Source":'',#
 	"LogTP":0	#写日志
 }
-
 
 if __name__=="__main__":
 	if len(sys.argv)==1:
