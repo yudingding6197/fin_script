7dcd35a18eec89c7b77d8fd817b0773bd24c654c
Bo Yu <yubo@kyntos.cn>
2019-04-09 09:02:57 +0800

    A

--- internal/dfcf_inf.py
+++ internal/dfcf_inf.py
@@ -309,18 +309,11 @@ def get_stk_code_by_cond(new_st_list, st='C', sr=-1, ps=80):
 
 def getSelfDefStock(stk_arr):
 	h_cook=\
-'st_pvi=04247319295955; qgqp_b_id=867a67ca83c13fbd622d079314b267df; \
-HAList=a-sz-002278-%u795E%u5F00%u80A1%u4EFD%2Ca-sz-002413-%u96F7%u79D1%u9632%u52A1%2Ca-sz-300291-%u534E%u5F55%u767E%u7EB3%2Ca-sh-603179\
--%u65B0%u6CC9%u80A1%u4EFD%2Ca-sz-300032-%u91D1%u9F99%u673A%u7535%2Ca-sz-002527-%u65B0%u65F6%u8FBE%2Ca-sh-603721-%u4E2D%u5E7F%u5929%u62E9%2Ca-\
-sz-300255-%u5E38%u5C71%u836F%u4E1A%2Ca-sz-002806-%u534E%u950B%u80A1%u4EFD%2Ca-sz-002622-%u878D%u94B0%u96C6%u56E2%2Ca-sh-601360-%u4E09%u516D%u96F6; \
-em_hq_fls=js; showpr3guide=1; ct=TlsXdMMkpaEgtaF4NkDFMKJ66FLGGD8j881eoI_rHG_gUf1D0kHm_vec7-r0MZwvzVPf-WbNGnCzTHpuJDJDFcFW56FJYLgxt0qHlb12xz4MuaUISb8zb\
-QwhF-Ua7eBDIqSEOLU_bgJDP4uXESnnLnGamF5EW00XEE_gYWgoSz4; ut=FobyicMgeV54OLFNgnrRk4dDkedquoSDr4ZAchNmAQHspWgUetdlESwn-ua44Ed4lpZY6csVsuiCle5qZxAuS8v-Inu\
-Fsm6TqHork_ycRSlV9xr-cxt2xU1WX-5dGcddr3_iauTIqMU26t7S1C8NNwQXAxsO9Dh_d1K9L5zZ8nXh9CQBC3s8LvRYENJG1g1rFBx1KTEQNNCjPGLCm4ELqpjOaxKCplgbSn7xxlvCROQZjpRD\
-cX1jLYzoIWnH4xbtc46ZBXyHKUKtSbyTZl6tnfhM49ukU31U; pi=6100112247957528%3byudingding6197%3bgoutou%3bk%2fYEChP6YGOVSiilXgkLUt7nPMUXog7ouaSWIqPFsIX0QZ0GrZ\
-V7TTysPbsXBELhUkb0jQX%2fkYiMwNTiTgKPnwe74xcogQI0ApV2140UQgeLzrliHuE4%2f3PRNnse9Ik1leQunYyxGxON4Pa9uSqQXVXS298RmzwZdPAPCmPMvNseTcAdHMiU0UzmrRDojkiwYuj\
-QtteO%3bfxWqv2yFNBeyKBtxLe7p0sWpBIj5jFARE8ErkhEMufyzRh05yHGLgzo5pXWxdE5lTzacI7b0QYG9AM69NtQy535QrVH%2fh2wYI9LU0IGcmOSFiM5qXRRh4TfmCJDwKl2HRUpqZ6tacDa\
-FLmeuZLUBzGdnNAJb%2bQ%3d%3d; uidal=6100112247957528goutou; sid=4725419; vtpst=|; em-quote-version=topspeed; EMSTtokenId=95b8128937a565c39526c705960e3be8;\
- st_si=96019977790553'
+'ct=0MQGTduqB7_LXzJefU6a84F3axofQVJHEBs6uOpXoMyFssYG4WXsYR9VRS2qe7xEjRbpI0x9NeBLzBXO2pSeXs-EUqnRH6u8\
+G0iKbyXg5WgHZa2DAPg8m5awyb1kz2T5cIf2Ai7eI9dxL2R5_xsHifIJ2Pq0zcUAR9LVVSlR7-I; ut=FobyicMgeV4UJna6Au6ASu7wtul_n26_zn\
+6kEQZ0cxP_TJaAGEHI10yGGvZqOGB_iuq8qRe-QXGC8BrY0FM7PobIY8SwUBNW2WeJuN65h0-s4vjZ_M7hL9xBF7lspjMki1CnUiFTfptLZJn1\
+mb8E_x9ZYWIhCyakraCd0Tl8EWqlDGefSBj6FjSec7PSwye3AgIhmlVY6VMeL3pRsVw6pNPcDG30oX38hevvlF8MMS-dCGgysgIVQtBZ_LfymTDGU\
+pxDm-U8wjNX4i6gUh6HGld3r1jNmdez; uidal=6100112247957528goutou; sid=4725419;'
 	 
 	prestr = 'getStockInfo'
 	urlall = 'http://myfavor1.eastmoney.com/mystock?f=gs&cb='+ prestr +'&g=41445869&0.617530589249439'
@@ -333,6 +326,7 @@ FLmeuZLUBzGdnNAJb%2bQ%3d%3d; uidal=6100112247957528goutou; sid=4725419; vtpst=|;
 	'Cookie': h_cook
 	}
 
+	#print(urlall)
 	res_data = None
 	try:
 		#·½·¨1
